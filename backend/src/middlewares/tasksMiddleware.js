6a3b9289f6c0bd8d277f3aa386d275da85f5cc32
BrunoSantosFF <brunosf1804@gmail.com>
2024-04-23 23:35:51 -0300

    validation :light:

---
+++ backend/src/middlewares/tasksMiddleware.js
@@ -0,0 +1,16 @@
+const validateBody = (request, response, next) => {
+  const { body } = request;
+
+  if (body.title === undefined) {
+    response.status(400).json({message : 'the field "title" is required'});
+  }
+  else if (body.title === ""){
+    response.status(400).json({message : 'title cannot be empty'});
+  }
+
+  next();
+}
+
+module.exports = {
+  validateBody,
+};
